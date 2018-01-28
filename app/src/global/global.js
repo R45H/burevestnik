9ac01df26fe2473e873233ea48c13d53ad2f18ba
R45H <gmr45h@gmail.com>
2018-01-28 20:57:21 +0300

    Some refactor code

--- app/src/global/global.js
+++ app/src/global/global.js
@@ -1,10 +1,11 @@
-// Для ссылок с href='#' и наличием только атрибутов class и href отключается стандартное поведение ссылки
+// Для ссылок с href='#' и наличием только атрибутов class, href и style отключается стандартное поведение ссылки
 $(document).on('click', 'a', function(event) {
 	var $this = $(this);
 
 	if ($this.attr('href') != '#') return;
 
 	if (
+		(this.attributes.length < 4 && $this.attr('class') && $this.attr('href') && $this.attr('style')) ||
 		(this.attributes.length < 3 && $this.attr('class') && $this.attr('href')) ||
 		(this.attributes.length < 2 && !$this.attr('class') && $this.attr('href'))
 	) {
